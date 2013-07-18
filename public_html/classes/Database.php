672ba625f375c9fa7c8675ea1bc6d7989df13c53
Richard Brinkman <r.brinkman@gmail.com>
2013-07-18 22:36:24 +0200

    ADD: Database class

---
+++ public_html/classes/Database.php
@@ -0,0 +1,11 @@
+<?php
+	class Database {
+		private static $db;
+
+		public static function getInstance() {
+			if (!isset(self::$db))
+				self::$db = new PDO("mysql:dbname=gallery;host=127.0.0.1", "php", "f2jffwjfw29134agfa");
+			return self::$db;
+		}
+	}
+?>
