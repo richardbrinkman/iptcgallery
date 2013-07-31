a0e40799f9457ad42ae041be2fbbe949a5552b8b
Richard Brinkman <r.brinkman@gmail.com>
2013-07-31 11:38:20 +0200

    FIX: (automatic) deletion of conditions

--- public_html/pages/Query.php
+++ public_html/pages/Query.php
@@ -1,6 +1,8 @@
 <?php
 	namespace pages;
 
+	define("debugmode", true);
+
 	class Query extends \classes\Page {
 		private $conditions;
 		private $drowpdownlist;
@@ -8,14 +10,17 @@
 		public function __construct() {
 			parent::__construct();
 			session_start();
+			//unset($_SESSION["conditions"]);
+			//unset($_SESSION["dropdownlist"]);
 			if (!isset($_SESSION["conditions"]))
 				$_SESSION["conditions"] = array();
 			if (!isset($_SESSION["dropdownlist"]))
 				$_SESSION["dropdownlist"] = array(
 					"logicalOperand" => array(),
 					"iptc" => array(),
-					"comparison" => array(),
-					"value" => array()
+					"comparisonOperand" => array(),
+					"value" => array(),
+					"common" => array()
 				);
 			$this->conditions = &$_SESSION["conditions"];
 			$this->dropdownlist = &$_SESSION["dropdownlist"];
@@ -36,8 +41,7 @@
 				$i = $_POST["i"];
 				
 				//delete all further conditions and dropdownlists
-				for ($j=$i + 1; $j<count($this->conditions); $j++) 
-					$this->delCondition($j);
+				$this->delCondition($i+1);
 
 				//logicalOperand changed
 				if ($this->conditions[$i][0] != $_POST["logicalOperand"]) {
@@ -63,6 +67,7 @@
 				//comparisonOperand changed
 				if ($this->conditions[$i][2] != $_POST["comparisonOperand"]) {
 					unset($this->dropdownlist["comparisonOperand"][$i]);
+					unset($this->dropdownlist["value"][$i]);
 					$this->conditions[$i][2] = $_POST["comparisonOperand"];
 				}
 			}
@@ -79,6 +84,19 @@
 				"WHERE true ";
 			$nameQueryEnd = 
 				"GROUP BY iptc_id ".
+				"HAVING numberOfValues > 1 ".
+				"ORDER BY iptc_name";
+
+			//Query for the common tags
+			$commonQueryStart =
+				"SELECT iptc_name, value ".
+				"FROM iptc ".
+				"NATURAL JOIN tag ".
+				"NATURAL JOIN link ".
+				"WHERE true ";
+			$commonQueryEnd =
+				"GROUP BY iptc_id ".
+				"HAVING COUNT(DISTINCT value) = 1 ".
 				"ORDER BY iptc_name";
 
 			//Query for the value dropdown list
@@ -109,28 +127,35 @@
 			foreach ($this->conditions as list($logicalOperand, $iptcId, $comparisonOperand, $value)) {
 				$i++;
 				
-				$result .= "<form method=\"post\" action=\"" . $_SERVER["PHP_SELF"] . "\">";
-				$result .= "<input type=\"hidden\" name=\"i\" value=\"$i\">";
-
-				//logicalOperand dropdown list
-				$result .= $this->getLogicalOperandDropdownlist($i);
-
 				//IPTC tag dropdown list
-				$result .= $this->getIptcDropdownlist($i, $nameQueryStart . $accumulatedCondition . $nameQueryEnd);
+				$iptcList = $this->getIptcDropdownlist($i, $nameQueryStart . $accumulatedCondition . $nameQueryEnd);
 
-				//ComparisonOperand dropdown list
-				$result .= $this->getComparisonDropdownlist($i);
+				if ($iptcList != "") {
+					$result .= "<form method=\"post\" action=\"" . $_SERVER["PHP_SELF"] . "\">";
+					$result .= "<input type=\"hidden\" name=\"i\" value=\"$i\">";
 
-				//value dropdown list
-				$result .= $this->getValueDropdownlist($i, $valueQueryStart . ($iptcId==0 ? "true " : "iptc_id='$iptcId' ") . $accumulatedCondition . $valueQueryEnd);
+					//logicalOperand dropdown list
+					$result .= $this->getLogicalOperandDropdownlist($i);
 
-				if (isset($comparisonOperand) && isset($value) && $value != "0")
-					$accumulatedCondition .= " $logicalOperand photo_id IN (SELECT photo_id FROM link WHERE tag_id $comparisonOperand '$value') ";
-				
-				$result .= "<a href=\"" . $_SERVER["PHP_SELF"] . "?del=$i\">delete</a>";
-				$result .= "</form>";
+					$result .= $iptcList;
+
+					//ComparisonOperand dropdown list
+					$result .= $this->getComparisonDropdownlist($i);
+
+					//value dropdown list
+					$result .= $this->getValueDropdownlist($i, $valueQueryStart . ($iptcId==0 ? "true " : "iptc_id='$iptcId' ") . $accumulatedCondition . $valueQueryEnd);
+
+					if (isset($comparisonOperand) && isset($value) && $value != "0")
+						$accumulatedCondition .= " $logicalOperand photo_id IN (SELECT photo_id FROM link WHERE tag_id $comparisonOperand $value) ";
+					
+					$result .= "<a href=\"" . $_SERVER["PHP_SELF"] . "?del=$i\">delete</a>";
+					$result .= "</form>";
+				}
 			}
 			$result .= "</div>";
+			
+			//Get common iptc tags
+			$result .= $this->getCommonTags($i, $commonQueryStart . $accumulatedCondition . $commonQueryEnd);
 	
 			$result .= $this->getThumbnails($photoQueryStart . $accumulatedCondition . $photoQueryEnd);
 
@@ -141,7 +166,7 @@
 			if (isset($this->dropdownlist["logicalOperand"][$i]))
 				return $this->dropdownlist["logicalOperand"][$i];
 			else {
-				$result = "<select name=\"logicalOperand\">";
+				$result = "<select name=\"logicalOperand\" onchange=\"submit()\">";
 				if (isset($this->conditions[$i][0]) && $this->conditions[$i][0] == "AND")
 					$result .= "<option value=\"AND\" selected>AND</option>";
 				else
@@ -160,28 +185,34 @@
 			if (isset($this->dropdownlist["iptc"][$i]))
 				return $this->dropdownlist["iptc"][$i];
 			else {
-				echo "<b>iptc:</b> $query<br>";
+				if (defined("debugmode"))
+					echo "<b>iptc:</b> $query<br>";
+				$foundSomething = false;
 				$iptcId = $this->conditions[$i][1];
 				$result = "<select name=\"iptc\" onchange=\"submit()\">";
 				$result .= "<option value=\"0\">Choose tag</option>";
 				foreach ($this->db->query($query) as list($iptc_id, $iptcName, $photos)) {
 					$selected = $iptcId==$iptc_id ? " selected" : "";
 					$result .= "<option value=\"$iptc_id\"$selected>$iptcName ($photos)</option>";
+					$foundSomething = true;
 				}
 				$result .= "</select>";
-				$this->dropdownlist["iptc"][$i] = $result;
-				return $result;
+				$this->dropdownlist["iptc"][$i] = $foundSomething ? $result : "";
+				return $this->dropdownlist["iptc"][$i];
 			}
 		}
 
 		private function getComparisonDropdownlist($i) {
-			if (isset($this->dropdownlist["comparison"][$i]))
-				return $this->dropdownlist["comparison"][$i];
+			if (isset($this->dropdownlist["comparisonOperand"][$i]))
+				return $this->dropdownlist["comparisonOperand"][$i];
 			else {
-				$result = "<select name=\"comparisonOperand\">";
-				$result .= "<option value=\"=\">=</option>";
+				$result = "<select name=\"comparisonOperand\" onchange=\"submit()\">";
+				foreach(array("=", "!=", "IS", "IS NOT", "LIKE", "NOT LIKE") as $operand) {
+					$selected = $this->conditions[$i][2] == $operand ? " selected" : "";
+					$result .= "<option value=\"$operand\"$selected>$operand</option>";
+				}
 				$result .= "</select>";
-				$this->dropdownlist["comparison"][$i] = $result;
+				$this->dropdownlist["comparisonOperand"][$i] = $result;
 				return $result;
 			}
 		}
@@ -190,25 +221,34 @@
 			if (isset($this->dropdownlist["value"][$i]))
 				return $this->dropdownlist["value"][$i];
 			else {
-				echo "<b>value:</b> $query<br>";
-				$result = "<select name=\"value\" onchange=\"submit()\">";
-				if ($this->conditions[$i][1] == "0") //no iptc value chosen
-					$result .= "<option value=\"0\">Choose tagname first</option>";
-				else {
-					$result .= "<option value=\"0\">Choose value</option>";
-					foreach($this->db->query($query) as list($tagId, $value, $numberOfPhotos)) {
-						$selected = $this->conditions[$i][3] == $tagId ? " selected" : "";
-						$result .= "<option value=\"$tagId\"$selected>$value ($numberOfPhotos)</option>";
+				if ($this->conditions[$i][2] == "LIKE" || $this->conditions[$i][2] == "NOT LIKE") {
+					$result = "<input type=\"text\" name=\"value\" onchange=\"submit()\" value=\"not implemented yet\">";
+					$result .= "<input type=\"submit\" value=\"submit\">";
+				} else {
+					$result = "<select name=\"value\" onchange=\"submit()\">";
+					if ($this->conditions[$i][1] == "0") //no iptc value chosen
+						$result .= "<option value=\"0\">Choose tagname first</option>";
+					else if ($this->conditions[$i][2] == "IS" || $this->conditions[$i][2] == "IS NOT")
+						$result .= "<option value=\"NULL\">NULL</option>";
+					else {
+						if (defined("debugmode"))
+							echo "<b>value:</b> $query<br>";
+						$result .= "<option value=\"0\">Choose value</option>";
+						foreach($this->db->query($query) as list($tagId, $value, $numberOfPhotos)) {
+							$selected = $this->conditions[$i][3] == $tagId ? " selected" : "";
+							$result .= "<option value=\"$tagId\"$selected>$value ($numberOfPhotos)</option>";
+						}
 					}
+					$result .= "</select>";
 				}
-				$result .= "</select>";
 				$this->dropdownlist["value"][$i] = $result;
 				return $result;
 			}
 		}
 
 		private function getThumbnails($query) {
-			echo "<b>thumbnails:</b> $query<br>";
+			if (defined("debugmode"))
+				echo "<b>thumbnails:</b> $query<br>";
 			$result = "<div class=\"thumbnails\" id=\"thumbnails\">";
 			foreach ($this->db->query($query) as list($photoId, $filename))
 				$result .= "<img src=\"/thumbnail.php?photo_id=$photoId\" title=\"$filename\" alt=\"$filename\">";
@@ -217,9 +257,28 @@
 		}
 
 		private function delCondition($i) {
-			array_splice($this->conditions, $i, 1);
-			foreach (array_keys($this->dropdownlist) as $key) {
-				array_splice($this->dropdownlist[$key], $i, 1);
+			for ($j=count($this->conditions)-1; $j>=$i; $j--)
+				array_pop($this->conditions);
+			foreach (array_keys($this->dropdownlist) as $key)
+				for ($j=count($this->dropdownlist[$key])-1; $j>=$i; $j--)
+					array_pop($this->dropdownlist[$key]);
+		}
+		
+		private function getCommonTags($i, $query) {
+			if (isset($this->dropdownlist["common"][$i]))
+				return $this->dropdownlist["common"][$i];
+			else {
+				if (defined("debugmode"))
+					echo "<b>common:</b> $query<br>";
+				$foundSomething = false;
+				$result = "<table>";
+				foreach ($this->db->query($query) as list($iptc_name, $value)) {
+					$result .= "<tr><td>$iptc_name</td><td>$value</td></tr>";
+					$foundSomething = true;
+				}
+				$result .= "</table>";
+				$this->dropdownlist["common"][$i] = $foundSomething ? $result : "";
+				return $this->dropdownlist["common"][$i];
 			}
 		}
 	}
